70a6a3d8f2e8fa7728a3c11f00ddd6cab9e758fa
christian.benalcazar <christian.benalcazar@registrocivil.gob.ec>
2022-04-21 18:28:19 -0500

    FEATURE: 1. Se agregan directorios del proyecto.

---
+++ src/main/java/ec/edu/cbenalcazar/exercisejava/ExerciseJavaMain.java
@@ -0,0 +1,15 @@
+package ec.edu.cbenalcazar.exercisejava;
+
+public class ExerciseJavaMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ejercicio Java con grafos");
+
+	}
+	
+	public void testMethod() {
+		System.out.println("Test method");
+	}
+
+}
---
+++ src/test/java/ec/edu/cbenalcazar/exercisejava/ExerciseJavaMainTest.java
@@ -0,0 +1,14 @@
+package ec.edu.cbenalcazar.exercisejava;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ExerciseJavaMainTest {
+
+	@Test
+	void testTestMethod() {
+		fail("Not yet implemented");
+	}
+
+}
